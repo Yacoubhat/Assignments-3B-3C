86b3e1b91917aed5d68e18375887491c762eb5a0
Yacoubhat <104165097@student.swin.edu.au>
2026-09-25 13:26:33 +1000

    Update d3.js

--- javaScripts/d3.js
+++ javaScripts/d3.js
@@ -32,7 +32,7 @@ svg.append("g")
     .attr("x", width / 2)
     .attr("y", 40)
     .style("fill", "white")
-    .style("font-size", "16px")
+    .style("font-size", "20px")
     .style("text-anchor", "middle")
     .style("font-weight", "bold")
     .text("Healthcare Expenditure per Capita (USD)");
